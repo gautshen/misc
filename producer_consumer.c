f57c4431fb12e5de18112fe8d8773476b160daff
Gautham R. Shenoy <ego@linux.vnet.ibm.com>
2020-07-15 12:36:26 +0530

    Add long options. And more hash-defines for caches. Fix memory leaks
    Signed-off-by: Gautham R. Shenoy <ego@linux.vnet.ibm.com>

--- producer_consumer.c
+++ producer_consumer.c
@@ -11,6 +11,7 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/time.h>
+#include <sys/cdefs.h>
 #include <limits.h>
 #include <sys/shm.h>
 #include <linux/futex.h>
@@ -25,9 +26,6 @@
 #else
 #define debug_printf(fmt...)
 #endif
-
-#define INDEX_ARRAY_SIZE (1024) //1k
-#define DATA_ARRAY_SIZE  (1024*1024*64) //512M
 
 /*********************** Perf related stuff ************************
  *
@@ -345,13 +343,18 @@ static void print_cache_details(unsigned long iter_diff)
 #endif
 }
 
+unsigned char stop = 0;
+
 static void sigalrm_handler(int junk)
 {
 	unsigned long i = iterations;
 	unsigned long long j = consumer_time_ns;
 	unsigned long iter_diff = i - iterations_prev;
 	unsigned long long time_ns_diff = j - consumer_time_ns_prev;
-	unsigned long long avg_time_ns = (time_ns_diff) / iter_diff;
+	unsigned long long avg_time_ns = 0;
+
+	if (iter_diff)
+		avg_time_ns = (time_ns_diff) / iter_diff;
 
 	printf("%8ld iterations, avg time:%6lld ns\n", iter_diff, avg_time_ns);
 	print_cache_details(iter_diff);
@@ -367,8 +370,33 @@ static void sigalrm_handler(int junk)
 
 static void sigusr1_handler(int junk)
 {
-	exit(0);
+	stop = 1;
 }
+
+#undef L1_CONTAINED
+
+#if defined(__PPC__)
+#define L1_CACHE_SHIFT		7
+#define L1_CACHE_SIZE		(32*1024)  //32K
+#define L2_CACHE_SIZE		(512*1024) //512K
+#else
+#define L1_CACHE_SHIFT	        6
+#define L1_CACHE_SIZE		(32*1024)
+#define L2_CACHE_SIZE		(256*1024)
+#endif
+
+#define	L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
+#define	SMP_CACHE_BYTES		L1_CACHE_BYTES
+#define ____cacheline_aligned __attribute__((__aligned__(SMP_CACHE_BYTES)))
+
+#ifdef L1_CONTAINED
+#define CACHE_SIZE		L1_CACHE_SIZE
+#else
+#define CACHE_SIZE		L2_CACHE_SIZE
+#endif
+
+#define INDEX_ARRAY_SIZE (CACHE_SIZE >> L1_CACHE_SHIFT) //
+#define DATA_ARRAY_SIZE  (INDEX_ARRAY_SIZE*1024)
 
 
 #define READ 0
@@ -379,12 +407,18 @@ static int pipe_fd2[2];
 
 char c;
 
+typedef unsigned long long u64;
+
+
+struct big_data {
+	u64 content;
+} ____cacheline_aligned;
 
 struct data_args {
 	unsigned long idx_arr_size;
 	unsigned long data_arr_size;
 	unsigned long *index_array;
-	unsigned long *data_array;
+	struct big_data *data_array;
 };
 
 static void *producer(void *arg)
@@ -394,7 +428,7 @@ static void *producer(void *arg)
 	unsigned long idx_arr_size = p->idx_arr_size;
 	unsigned long data_arr_size = p->data_arr_size;
 	unsigned long *index_array = p->index_array;
-	unsigned long *data_array = p->data_array;
+	struct big_data *data_array = p->data_array;
 	pthread_t thread = pthread_self();
         cpu_set_t cpuset;
 
@@ -414,7 +448,7 @@ static void *producer(void *arg)
 	signal(SIGALRM, sigalrm_handler);
 	alarm(1);
 
-	while (1) {
+	while (!stop) {
 
 		debug_printf("Producer while begin\n");
 		/*
@@ -427,13 +461,12 @@ static void *producer(void *arg)
 			unsigned long data;
 
 			idx = random() % data_arr_size;
-			idx += 128;
 			data = random() % UINT_MAX;
 
 			debug_printf("Producer : [%d] = %ld,  [%ld] = 0x%llx\n",
 				i, idx, idx, data);
 			index_array[i] = idx;
-			data_array[idx] = data;
+			data_array[idx].content = data;
 		}
 
 		debug_printf("Producer writing to pipe\n");
@@ -476,7 +509,7 @@ static void *consumer(void *arg)
 	unsigned long idx_arr_size = con->idx_arr_size;
 	unsigned long data_arr_size = con->data_arr_size;
 	unsigned long *index_array = con->index_array;
-	unsigned long *data_array = con->data_array;
+	struct big_data *data_array = con->data_array;
 	pthread_t thread = pthread_self();
         cpu_set_t cpuset;
 
@@ -495,7 +528,7 @@ static void *consumer(void *arg)
 		index_array, data_array);
 
 	setup_counters();
-	while (1) {
+	while (!stop) {
 		unsigned long idx = 0;
 		volatile unsigned int sum = 0;
 		struct timespec begin, end;
@@ -515,7 +548,7 @@ static void *consumer(void *arg)
 			unsigned long data;
 
 			idx = index_array[i];
-			data = data_array[idx];
+			data = data_array[idx].content;
 
 			debug_printf("Consumer : [%d] = %ld,  [%ld] = 0x%llx\n",
 				i, idx, idx, data);
@@ -540,7 +573,7 @@ update_done:
 		reset_counters();
 		idx = 0;
 		debug_printf("Consumer writing [%ld] = 0x%llx\n", idx, sum);
-		data_array[idx] = sum;
+		data_array[idx].content = sum;
 
 		debug_printf("Consumer writing to pipe\n");
 		assert(write(pipe_fd1[WRITE], &c, 1) == 1);
@@ -550,40 +583,164 @@ update_done:
 }
 
 
+int idx_arr_size = INDEX_ARRAY_SIZE;
+int data_arr_size =  DATA_ARRAY_SIZE;
+int cpu_producer = -1, cpu_consumer = -1;
+unsigned long seed = 6407741;
+unsigned long cache_size = CACHE_SIZE;
+unsigned long *idx_arr;
+struct big_data *data_arr;
+
+void print_usage(int argc, char *argv[])
+{
+	printf("Usage: %s [OPTIONS]\n", argv[0]);
+	printf("Following options are available\n");
+	printf("-p, --pcpu\t\t\t The CPU to which the producer should be affined\n");
+	printf("-c, --ccpu\t\t\t The CPU to which the producer should be affined\n");
+	printf("-r, --random-seed\t\t The seed used for random number generation\n");
+	printf("-l, --iteration-length\t\t The number of loads per consumer-iteration\n");
+	printf("-s, --cache-size\t\t Size of the cache in bytes.\n");
+	printf("-x, --timeout\t\t\t Number of seconds to run the benchmark\n");	
+	printf("Note : Atmost one of --iteration-length or --cache-size can be provided\n");
+}
+
+void parse_args(int argc, char *argv[])
+{
+	int c;
+
+	int iteration_length_provided = 0;
+	int cache_size_provided = 0;
+
+	while(1) {
+		static struct option long_options[] = {
+			{"pcpu", required_argument, 0, 'p'},
+			{"ccpu", required_argument, 0, 'c'},
+			{"random-seed", required_argument, 0, 'r'},
+			{"iteration-length", required_argument, 0, 'l'},
+			{"cache-size", required_argument, 0, 's'},
+			{"timeout", required_argument, 0, 't'},
+			{"help", no_argument, 0, 'h'},
+			{0, 0, 0, 0},
+		};
+
+		int option_index = 0;
+
+		c = getopt_long(argc, argv, "hp:c:r:l:s:t:", long_options, &option_index);
+
+		/* Options are done */
+		if (c == -1)
+			break;
+		switch (c) {
+		case 'h':
+			print_usage(argc, argv);
+			exit(0);
+
+		case 'p':
+			cpu_producer = (int) strtoul(optarg, NULL, 10);
+			break;
+
+		case 'c':
+			cpu_consumer = (int) strtoul(optarg, NULL, 10);
+			break;
+
+		case 'r':
+			seed = strtoul(optarg, NULL, 10);
+			break;
+
+		case 'l':
+			if (cache_size_provided) {
+				printf("Only one of --iteration-length and --cache-size should be provided\n");
+				exit(1);
+			}
+
+			iteration_length_provided = 1;
+			idx_arr_size = strtoul(optarg, NULL, 10);
+			break;
+
+		case 's':
+			if (iteration_length_provided) {
+				printf("Only one of --iteration-length and --cache-size should be provided\n");
+				exit(1);
+			}
+			cache_size_provided = 1;
+			cache_size = strtoul(optarg, NULL, 10);
+			idx_arr_size = cache_size >> L1_CACHE_SHIFT;
+			break;
+
+		case 't':
+			timeout = strtoul(optarg, NULL, 10);
+			break;
+
+		default:
+			printf("Invalid Options\n");
+			print_usage(argc, argv);
+			exit(1);
+		}
+	}
+}
+
+pthread_t create_thread(const char *name, pthread_attr_t *attr, void *(*fn)(void *),
+			struct data_args *args, int cpu)
+			/* unsigned long idx_arr_size, unsigned long data_arr_size, */
+			/* unsigned long *idx_arr, struct big_data *data_arr) */
+{
+	pthread_t tid;
+	cpu_set_t cpuset;
+
+        args->idx_arr_size = idx_arr_size;
+	args->index_array = idx_arr;
+
+	args->data_arr_size = data_arr_size;
+	args->data_array = data_arr;
+
+	pthread_attr_init(attr);
+	if (cpu != -1) {
+		CPU_ZERO(&cpuset);
+		CPU_SET(cpu, &cpuset);
+		if (pthread_attr_setaffinity_np(attr,
+						sizeof(cpu_set_t),
+						&cpuset)) {
+			perror("Error setting affinity for producer");
+			exit(1);
+		}
+	}
+
+	if (pthread_create(&tid, attr, fn, args)) {
+		printf("Error creating the %s\n", name);
+		exit(1);
+	} else {
+		printf("%s created with tid %ld\n", name, tid);
+	}
+
+	return tid;
+}
+
 int main(int argc, char *argv[])
 {
 	signed char c;
 	pthread_t producer_tid, consumer_tid;	
 	pthread_attr_t producer_attr, consumer_attr;
 	struct data_args producer_args, consumer_args;
-	unsigned long *idx_arr, *data_arr;
-	int idx_arr_size = INDEX_ARRAY_SIZE;
-	int data_arr_size =  DATA_ARRAY_SIZE;
-	int cpu_producer = -1, cpu_consumer = -1;
-	unsigned long seed = 6407741;
-	cpu_set_t cpuset;
-	
 
-	if (argc == 2 || argc == 4 || argc == 5) {
-		seed = (unsigned int) strtoul(argv[1], NULL, 10);
-	}
+	parse_args(argc, argv);
 
-	if (argc == 4 || argc == 5) {
-		cpu_producer = (unsigned int) strtoul(argv[2], NULL, 10);
-		cpu_consumer = (unsigned int) strtoul(argv[3], NULL, 10);
-	}
+	if (idx_arr_size * 1024 < DATA_ARRAY_SIZE)
+		data_arr_size = idx_arr_size * 1024;
 
-	if (argc == 5) {
-		timeout = (unsigned int) strtoul(argv[4], NULL, 10);
-	}
+	printf("seed = %ld\n", seed);
+	printf("Size of cacheline = %d bytes\n", sizeof(struct big_data));
+	printf("Number of indices in an iteration = %d\n", idx_arr_size);
+	printf("Data array size = %ld indices x %ld bytes = %lld bytes\n",
+	       data_arr_size, sizeof(struct big_data),
+	       data_arr_size * sizeof(struct big_data));
 
 	srandom(seed);
+
 	if (pipe(pipe_fd1) || pipe(pipe_fd2)) {
 		printf("Error creating pipes\n");
 		exit(1);
 	}
 
-	
 	idx_arr = malloc(idx_arr_size * sizeof(unsigned long));
 
 	if (!idx_arr) {
@@ -591,63 +748,31 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	producer_args.idx_arr_size = consumer_args.idx_arr_size = idx_arr_size;
-	producer_args.index_array = idx_arr;
-	consumer_args.index_array = idx_arr;
+	printf("idx_arr = 0x%llx\n", idx_arr);
 
-	data_arr = malloc(data_arr_size * sizeof(unsigned long));
+	data_arr = malloc(data_arr_size * sizeof(struct big_data));
 	if (!data_arr) {
 		printf("Not enough memory for allocating an data array\n");
 		exit(1);
 	}
-
-	producer_args.data_arr_size = consumer_args.data_arr_size = data_arr_size;
-	producer_args.data_array = data_arr;
-	consumer_args.data_array = data_arr;
+	printf("data_arr = 0x%llx\n", data_arr);
 
 	setpgid(getpid(), getpid());
 	signal(SIGUSR1, sigusr1_handler);
 
-	pthread_attr_init(&producer_attr);
-	if (cpu_producer != -1) {
-		CPU_ZERO(&cpuset);
-		CPU_SET(cpu_producer, &cpuset);
-		if (pthread_attr_setaffinity_np(&producer_attr,
-						sizeof(cpu_set_t),
-						&cpuset)) {
-			perror("Error setting affinity for producer");
-			exit(1);
-		}
-		//printf("Affined producer to CPU %d\n", cpu_producer);
-	}
-	
-	if (pthread_create(&producer_tid,
-			   &producer_attr, producer, &producer_args)) {
-		printf("Error creating the producer\n");
-		exit(1);
-	}
+	producer_tid = create_thread("producer", &producer_attr,
+				     producer, &producer_args, cpu_producer);
 
-	pthread_attr_init(&consumer_attr);
-	if (cpu_consumer != -1) {
-		CPU_ZERO(&cpuset);
-		CPU_SET(cpu_consumer, &cpuset);
-		if (pthread_attr_setaffinity_np(&consumer_attr,
-						sizeof(cpu_set_t),
-						&cpuset)) {
-			printf("Error setting affinity for consumer");
-			exit(1);
-		}
-		//printf("Affined consumer to CPU %d\n", cpu_consumer);
-	}
+	consumer_tid = create_thread("consumer", &consumer_attr,
+				     consumer, &consumer_args, cpu_consumer);
 
-	if (pthread_create(&consumer_tid,
-			   &consumer_attr, consumer, &consumer_args)) {
-		printf("Error creating the consumer\n");
-		exit(1);
-	}
+	pthread_join(producer_tid, NULL);
+	pthread_join(consumer_tid, NULL);
 
-	while (1)
-		sleep(3600);
+	pthread_attr_destroy(&producer_attr);
+	pthread_attr_destroy(&consumer_attr);
+	free(idx_arr);
+	free(data_arr);
 
 	return 0;
 }
